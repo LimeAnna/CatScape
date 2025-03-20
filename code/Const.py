9b615a3e6111f146e8bdebfb1652113c444657ff
AnnaLima <gwylinswan@gmail.com>
2025-03-19 22:31:40 -0300

    Menu, imagem e música

---
+++ code/Const.py
@@ -0,0 +1,13 @@
+# C
+COLOR_PINK = (255, 20, 147)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 600
+WIN_HEIGHT = 338
