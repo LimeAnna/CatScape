1a66136d1b8d7d203bcfbc6d611c8fed610b0ff6
AnnaLima <gwylinswan@gmail.com>
2025-03-20 23:22:11 -0300

    Inclusao do level e interação do menu

--- code/Const.py
+++ code/Const.py
@@ -3,10 +3,19 @@ COLOR_PINK = (75, 42, 45)
 COLOR_WHITE = (255, 255, 255)
 
 # M
-MENU_OPTION = ('NEW GAME 1P',
-               'NEW GAME 2P - COOPERATIVE',
+MENU_OPTION = ('NEW GAME',
                'SCORE',
                'EXIT')
+
+#E
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+}
 
 # W
 WIN_WIDTH = 600
--- code/Game.py
+++ code/Game.py
@@ -1,6 +1,7 @@
 import pygame
 
-from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.Level import Level
 # cat scape
 from code.Menu import Menu
 
@@ -14,8 +15,16 @@ class Game:
     def run(self, ):
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
+
+            if menu_return == MENU_OPTION[0]:
+                level = Level(self.window, 'Level1Bg', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[2]:
+                pygame.quit()
+                quit()
+            else:
+                pass
 
 
 
